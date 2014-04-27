5a4a5e0cfc038277bf15c66f609fa769db725b04
michael <swarzef@gmail.com>
2014-04-27 18:05:00 +0200

    EntryCards in

--- entrycard.py
+++ entrycard.py
@@ -1,28 +1,20 @@
 cardlist = []
 
 def start():
-    questions = []
-    answers = []
-    for Card in cardlist:
-        questions.append(Card.question)
-        answers.append(Card.ans)
-    iter_q = iter(questions)
-    iter_a = iter(answers)
+    iter_cardlist = iter(cardlist)
 
-    question = next(iter_q)
-    answer = next(iter_a)
-    print(answer)
-    revealed = 0 #to prevent reveal spamming
+    Card = next(iter_cardlist)
+
+    revealed = 0 #to prevent reveal spamming  
 
     import tkinter as tk
 
     def verify():
-        nonlocal answer
-        print(answer)
+        nonlocal Card
         nonlocal revealed
         if revealed == 0:
             print(EntryField.get())
-            if EntryField.get() == answer:
+            if EntryField.get() == Card.answer:
                 tk.Label(AnsFrame, text='Correct').pack()
                 ask_to_reveal()
             else:
@@ -34,14 +26,14 @@ def start():
         AnsFrame.destroy()
         AnsFrame = tk.Frame(Root)
         AnsFrame.pack()
-        nonlocal answer
-        tk.Label(AnsFrame, text=answer, font = ('times', 12, 'bold')).pack()
+        nonlocal Card
+        tk.Label(AnsFrame, text=Card.answer, font = ('times', 12, 'bold')).pack()
         EntryField.delete(0,len(EntryField.get()))
         EntryField['state'] = tk.DISABLED
         nonlocal revealed
         revealed = 1
         def fun(i):
-            rate(QLab['text'], i)
+            rate(i)
         for i in range(0,6):
             _1 = tk.Frame(AnsFrame)
             _1.pack(side=tk.LEFT)
@@ -51,34 +43,32 @@ def start():
                       command = lambda i=i: fun(i))\
               .pack(side=tk.TOP)
 
-    def rate(q,x):
+    def rate(x):
         nonlocal revealed
         nonlocal AnsFrame
         if revealed == 1:
-            print(q, 'scored', x)
+            print(Card, 'scored', x)
+            Card.set_interval(x)
             revealed = 0
             AnsFrame.destroy()
             AnsFrame = tk.Frame(Root)
             AnsFrame.pack()
             EntryField['state'] = tk.NORMAL
             if x in [0,1,2]:
-                questions.append(q)
-                answers.append(answers[questions.index(q)])
-            nonlocal question
-            nonlocal answer
+                cardlist.append(Card)
             try:
-                QLab['text'] = next(iter_q)
-                answer = next(iter_a)
+                nonlocal Card
+                Card = next(iter_cardlist)
+                QLab['text'] = Card.question
             except StopIteration:
                 EntryField.destroy()
                 QLab['text'] = 'You\'re done'
                 Root.bind('<KP_Enter>', lambda dummy: Root.destroy())
                 Root.bind('<Return>', lambda dummy: Root.destroy())
                 Root.bind('<space>', lambda dummy: Root.destroy())
-        
 
     Root = tk.Tk()
-    QLab = tk.Label(Root, text = question)
+    QLab = tk.Label(Root, text = Card.question)
     QLab.pack()
     QLab['font'] = ('times', 20, 'bold')
     EntryField = tk.Entry(Root)
@@ -86,8 +76,8 @@ def start():
     EntryField.focus()
 
     for x in range(0,6):
-        Root.bind('<KP_'+str(x)+'>', lambda dummy, x=x: rate(QLab['text'], x))
-        Root.bind(str(x), lambda dummy, x = x: rate(QLab['text'], x))
+        Root.bind('<KP_'+str(x)+'>', lambda dummy, x=x: rate(x))
+        Root.bind(str(x), lambda dummy, x = x: rate(x))
     Root.bind('<KP_Enter>', lambda dummy: verify())
     Root.bind('<Return>', lambda dummy: verify())
     Root.bind('<Control_L><r>', lambda dummy: ask_to_reveal())
