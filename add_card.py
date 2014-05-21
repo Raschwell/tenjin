8ba43e81450d5533cf1ab19c2f21a2dfe0b33b5a
Raschwell <swarzef@gmail.com>
2014-05-21 23:01:25 +0200

    Relocated files, for use outside devel folder

--- add_card.py
+++ add_card.py
@@ -7,13 +7,17 @@ question = sys.argv[3]
 answer = sys.argv[4]
 specific = sys.argv[5:]
 
+config = configparser.ConfigParser()
+config.read_file(open('/usr/home/michael/.config/tenjin/config'))
+location = config['Main']['location']
+
 carddb = configparser.ConfigParser()
 
-carddb.read_file(open('cards.db'))
+carddb.read_file(open(location))
 if ID not in carddb:
     carddb[ID] = {'type' : cardtype, 'question' : question, 'answer' : answer}
     print('Added new card:', ID)
-    with open('cards.db', 'w') as configfile:
+    with open(location, 'w') as configfile:
         carddb.write(configfile)
 else:
     print('CardID already in use')
